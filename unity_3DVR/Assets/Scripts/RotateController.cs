55f5e9393a836d68630c69837e39d9e10bd13c0b
HWChia <66512878+xmas21@users.noreply.github.com>
2020-09-19 17:04:06 +0800

    VR基礎設定

---
+++ unity_3DVR/Assets/Scripts/RotateController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateController : MonoBehaviour
+{
+    [Header("旋轉角度"), Range(0, 180)]
+    public float rotare = 90;
+    [Header("旋轉速度"), Range(0, 50)]
+    public float speed = 1.5f;
+
+    void StartRotate()
+    {
+        
+    }
+
+}
